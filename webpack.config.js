64d38df55d7dfd8425ab0f61a701eb77dbeb1bf8
Reggeditt <tkodjosarso@gmail.com>
2023-02-20 20:36:29 +0000

    setup dev server

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: "./dist",
+  },
   module: {
     rules: [
       {
